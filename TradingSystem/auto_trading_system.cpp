416612d3dd11f717081d46117d5daffa1b0ba612
soyokaze <soyokaze86@naver.com>
2026-07-03 12:49:07 +0900

    Auto Trading System – 기본 기능

--- TradingSystem/auto_trading_system.cpp
+++ TradingSystem/auto_trading_system.cpp
@@ -5,19 +5,20 @@ void AutoTradingSystem::selectStockBroker(std::unique_ptr<IStockBrokerDriver> dr
 }
 
 void AutoTradingSystem::login(const std::string& id, const std::string& password) {
-
+	if (driver) driver->login(id, pass);
 }
 
 void AutoTradingSystem::buy(const std::string& stockCode, int price, int count) {
-
+	if (driver) driver->buy(stockCode, price, count);
 }
 
 void AutoTradingSystem::sell(const std::string& stockCode, int price, int count) {
-
+	if (driver) m_broker->sell(stockCode, price, count);
 }
 
 int AutoTradingSystem::getPrice(const std::string& stockCode) {
-
+	if (driver) return driver->getPrice(stockCode);
+	return 0;
 }
 
 void AutoTradingSystem::buyNiceTiming(const std::string& stockCode, int totalMoney) {
